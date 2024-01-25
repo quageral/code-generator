080ddd3147295f6406472b919df2bb38868ffa85
quageral <1113054898@qq.com>
2024-01-25 11:45:13 +0800

    meta info manage

---
+++ code-generator-maker/src/main/java/com/code/maker/meta/MetaManager.java
@@ -0,0 +1,33 @@
+package com.code.maker.meta;
+
+import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.json.JSONUtil;
+
+/**
+ * @author ctc
+ * @date 2024/1/23
+ */
+public class MetaManager {
+
+    private static volatile Meta meta;
+
+    public static Meta getMetaObject() {
+        // 双重检查锁
+        if (meta == null) {
+            synchronized (MetaManager.class) {
+                if (meta == null) {
+                    meta = initMeta();
+                }
+            }
+        }
+        return meta;
+    }
+
+    public static Meta initMeta() {
+        String metaJson = ResourceUtil.readUtf8Str("meta.json");
+        Meta newMeta = JSONUtil.toBean(metaJson, Meta.class);
+        // TODO: 校验配置文件参数合法性
+        return newMeta;
+    }
+
+}
