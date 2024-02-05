21360779a5f86082697af8b96575a5bf9110b09d
quageral <1113054898@qq.com>
2024-02-05 18:42:40 +0800

    version 3.1 develop based on frontend template pages: register; login; user/generator list; search

---
+++ code-generator-web-frontend/config/routes.ts
@@ -0,0 +1,25 @@
+export default [
+  { path: '/user', layout: false,
+    routes: [
+      { path: '/user/login', component: './User/Login' },
+      { path: '/user/register', component: './User/Register' },
+    ] },
+  {
+    path: '/admin',
+    icon: 'crown',
+    name: '管理页',
+    access: 'canAdmin',
+    routes: [
+      { path: '/admin', redirect: '/admin/user' },
+      { icon: 'table', path: '/admin/user', component: './Admin/User', name: '用户管理' },
+      {
+        icon: 'tools',
+        path: '/admin/generator',
+        component: './Admin/Generator',
+        name: '生成器管理',
+      },
+    ],
+  },
+  { path: '/', icon: 'home', component: './Index', name: '主页' },
+  { path: '*', layout: false, component: './404' },
+];
